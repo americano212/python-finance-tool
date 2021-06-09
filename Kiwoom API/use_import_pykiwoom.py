65c3dac4bbcc396ea5645598697987d66e4edcde
wq0212 <wq0212@naver.com>
2021-06-09 18:05:21 +0900

    update kw api part

--- Kiwoom API/use_import_pykiwoom.py
+++ Kiwoom API/use_import_pykiwoom.py
@@ -22,4 +22,13 @@ accounts = kiwoom.GetLoginInfo("ACCNO")
 stock_account = accounts[0]
 
 # 삼성전자, 10주, 시장가주문 매수
-kiwoom.SendOrder("시장가매수", "0101", stock_account, 1, "005930", 10, 0, "03", "")
+#kiwoom.SendOrder("시장가매수", "0101", stock_account, 1, "005930", 10, 0, "03", "")
+
+
+kospi = kiwoom.GetCodeListByMarket('0')
+kosdaq = kiwoom.GetCodeListByMarket('10')
+etf = kiwoom.GetCodeListByMarket('8')
+
+print(len(kospi), kospi)
+print(len(kosdaq), kosdaq)
+print(len(etf), etf)
